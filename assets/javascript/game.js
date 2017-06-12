6d63ac881e9c844856622644a272084cec39401b
Tyler J. Frega <tyfrega@gmail.com>
2017-06-12 02:49:52 -0400

    finally got the bloddy dashes to disply

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,99 @@
+	//tracks keystrokes logs if letter is contained on the word
+	document.onkeyup = function logKey(event){
+		var word = game.currentWord[0];
+		var letter = event.key;
+
+
+		for(i = 0; i < word.length; i++){
+			if (letter === word.charAt(i)){
+				console.log("it worked");
+
+			}
+		}
+	}
+
 var game = {
 
 	wordBank: ['horse', 'cowboy', 'gunslinger', 'indian', 'sheriff'],
 	currentWord: [],
+	answerArray: [],
 
-	//function to produce random word and makes word push word to currentWord array
+	//function to produce random word and push word to currentWord array
 	getWord: function(){
 		var word = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
 		this.currentWord.push(word);
 		},
 	//displays dashes for each letter of word	
-	displayWord: function(){
+	getDashes: function(){
 		for(i = 0; i < this.currentWord[0].length; i++){
-			document.write("- ");
+			this.answerArray[i] = "_";		
 		}
+	},
+
+	displayDashes: function(){
+		var el = document.getElementById('letters');
+		el.innerHTML = this.answerArray.join(" ");
+	},
+
+	gameLoop: function(){
+		var remainingLetters = currentWord.length;
+		while(remainingLetters > 0){
+			var el = document.getElementById('letters');
+			el.innerHTML = this.answerArray.join(" ");
+		}
+
 	}
 
 	};
 
 	game.getWord();
-	game.displayWord();
-
-	//tracks keystrokes logs if letter is contained on the word
-	document.onkeyup = function(event){
-		var word = game.currentWord[0];
-		var letter = event.key;
-
-		for(i = 0; i < word.length; i++){
-			if (letter === word.charAt(i)){
-				console.log("it worked");
-			}
-		}
-	}
+	game.getDashes();
+	game.displayDashes();
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+// var wordBank = ['horse', 'cowboy', 'gunslinger', 'indian', 'sheriff'];
+
+// var word = wordBank[Math.floor(Math.random() * wordBank.length )];
+
+// var answerArray = [];
+
+// for(i = 0; i< word.length; i++){
+// 	answerArray[i] = "_";
+// }
+
+// var remainingLetters = word.length;
+
+// while(remainingLetters > 0) {
+// 	var el = document.getElementById('letters');
+// 	el.innerHTML = answerArray.join("");
+
+// 	var guess = prompt("guess a letter");
+// 	if(guess === null){
+// 		break;
+// 	}else if (guess.length !== 1){
+// 		alert("enter a single letter");
+// 	}else {
+// 		for(i = 0; i < word.length; i++){
+// 			if(word[i] === guess){
+// 				answerArray[i] = guess;
+// 				remainingLetters--;
+// 			}
+// 		}
+// 	}
+// }
+
+
+// el.innerHTML = answerArray.join(" ");
+// alert("you win");
+
